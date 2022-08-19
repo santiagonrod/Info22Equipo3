16956aa6ce6667ca8b1838636b2ff7ae15af4103
juani021298@gmail.com <juani021298@gmail.com>
2022-08-19 16:09:12 -0300

    AHORA SIIIIIII

--- source/ProyectoFinal/settings/base.py
+++ source/ProyectoFinal/settings/base.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProyectoFinal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Proyecto',
+        'USER': 'postgres',
+        'PASSWORD': 'equipo32022',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
