b9ea7963ae3a02d5ff7e0ee873e51fef7b57643b
Your Name <you@example.com>
2022-08-19 14:59:17 -0300

    a

--- source/apps/blog/models.py
+++ source/apps/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Noticia(models.Model):
+class post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -11,9 +11,12 @@ class Noticia(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+    
