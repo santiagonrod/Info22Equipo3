7e1841979547756fe99545ac9eb743a61313f2a5
MalenaDomenech <domenechmalena0@gmail.com>
2022-08-25 21:23:50 -0300

    nuevo formulario para post

--- source/apps/blog/forms.py
+++ source/apps/blog/forms.py
@@ -1,7 +1,10 @@
+from dataclasses import fields
 from socket import fromshare
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import post
+
 class CreacionDeUsuario(UserCreationForm):
     email = forms.EmailField()
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
@@ -11,3 +14,8 @@ class CreacionDeUsuario(UserCreationForm):
         model=User
         fields=['username','email','password1','password2']
         help_texts={k:"" for k in fields}
+
+class FormPost(forms.ModelForm):
+    class Meta:
+        model = post
+        fields = ('title', 'text', 'imagen')
