509dfdd3732f429f3e9de042142c72d24f82a3e9
Santiago <santiago.nrodriguez@hotmail.com>
2022-08-30 12:33:56 -0300

    carga de posteos desde db

--- source/apps/blog/urls.py
+++ source/apps/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('registro/',views.registro, name='registro'),
     path('nuevo_post/', views.crear_post, name='nuevo_post'),
     path('sobre_nosotros/', views.sobre_nosotros, name='sobre_nosotros')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
